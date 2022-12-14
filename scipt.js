5e12e13c23ec095f2d6100cb0bd1dab3c631614e
bishwajit hazra <bishwajithazra75478@gmail.com>
2022-12-14 18:49:10 +0600

    all area completed

---
+++ scipt.js
@@ -0,0 +1,151 @@
+let firstName = "asif";
+let age = 44;
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(age);
+
+//1. object
+//2.primitive
+
+/*
+1.Number
+2.String
+3.Boolean
+----------
+4.Undefined
+5.Null
+
+----------Later
+6.symble
+7.Bigint
+*/
+
+//1. Namber
+let age = 21;
+console.log(age);
+
+// 2.bString
+let firstName = "ASIF 22";
+console.log(firstName);
+
+//3.Boolean
+let issinger = true;
+console.log(issinger);
+
+//4. Undefined
+let homecity;
+console.log(homecity);
+
+//5. Null
+let incom = null;
+console.log(incom);
+
+console.log(age);
+
+console.log(typeof age);
+console.log(typeof issinger);
+console.log(typeof incom); // javascipt bug
+
+console.log(homecity);
+homecity = "daka";
+console.log(homecity);
+
+console.log(typeof issinger);
+issinger = "Bangladeshi singer";
+console.log(typeof issinger);
+console.log(typeof incom); // javascipt bug
+
+let firstName = "Asif";
+
+/*
+1. let
+2. const
+3. var
+*/
+
+//1. let.......
+let myage = 21;
+console.log(myage);
+myage = 40;
+console.log(myage);
+
+//2. const........
+const birthday = 2002;
+console.log(birthday);
+
+//3. var...........
+var job = "Tacher";
+console.log(job);
+
+//javascipt opertors basic
+console.log(18 + 40);
+let profit = 70 - 40;
+console.log(profit);
+
+let currenyear = 2023;
+
+let myage = currenyear - 2002;
+console.log(myage);
+
+let kaka = currenyear - 1991;
+console.log(kaka);
+
+console.log(myage, kaka);
+
+let agediff = myage - kaka;
+console.log(agediff);
+
+// +
+// -
+// /
+// **
+// %
+//++
+//__
+
+console.log(90 + 10);
+console.log(50 - 25);
+console.log(30 / 5);
+console.log(4 ** 4);
+console.log(23 % 9);
+
+let x = 10;
+x++;
+console.log(x);
+
+let y = 30;
+y--;
+console.log(y);
+
+console.log;
+let firstname = "sakib ";
+// let lastName = "rox";
+
+console.log(firstname + lastName);
+
+// =
+// +=
+// -=
+// *=
+// /=
+
+let item = 10 + 5;
+console.log(item);
+
+let x = 15;
+x += 10; //x = x + 10;
+console.log(x);
+
+let y = 30;
+y -= 12; //y = y- 12;
+console.log(y);
+
+let z = 5;
+z *= 5; // z = z * 5;
+z = 5 * 5;
+console.log(z);
+
+let w = 15;
+w /= 3; //w = w /3 ;
+console.log(w);
